7d4964b62e9f5a9d936a5c6524d3f1b86e8db189
asvol <248997@gmail.com>
2021-08-31 19:38:14 +0500

    Add parser RTCMv3 class

--- src/Asv.Gnss/Class1.cs
+++ src/Asv.Gnss/Class1.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Asv.Gnss
 {
-    public class Class1
+
+    public interface IGnssParser
     {
+        bool Read(byte data);
+        void Reset();
+    }
+
+    public enum RtcmV3ParserState
+    {
+        S1_Sync,
+
+    }
+
+    public class RtcmV3Parser:IGnssParser
+    {
+        private const byte Rtcm3Preamb = 0xD3;
+
+        public byte[] _buffer { get; } = new byte[1024 * 4];
+
+        private RtcmV3ParserState _state;
+
+        public bool Read(byte data)
+        {
+            switch (_state)
+            {
+                case RtcmV3ParserState.S1_Sync:
+                    if (data == Rtcm3Preamb)
+                    {
+                        _state = 1;
+                        _buffer[0] = data;
+                    }
+                    
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
