235c29c212bc1922a57d5a8d7d6c3992685da17b
asvol <248997@gmail.com>
2021-08-31 15:13:23 +0500

    Init commit

---
+++ src/Asv.Gnss/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asv.Gnss
+{
+    public class Class1
+    {
+    }
+}
