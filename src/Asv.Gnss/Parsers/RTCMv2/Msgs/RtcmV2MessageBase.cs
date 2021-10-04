418b6a73d40c8e92af27e5b70fc42d63eb7e7428
malobanov <lobanov@nports.ru>
2021-10-04 11:18:34 +0500

    [RTCMv2] Added new parser, RTCMv2

---
+++ src/Asv.Gnss/Parsers/RTCMv2/Msgs/RtcmV2MessageBase.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+namespace Asv.Gnss
+{
+    public abstract class RtcmV2MessageBase : GnssMessageBase
+    {
+        protected virtual DateTime adjhour(double zcnt)
+        {
+            var utc = DateTime.UtcNow;
+            double tow = 0;
+            var week = 0;
+
+            /* if no time, get cpu time */
+            var time = RtcmV3Helper.Utc2Gps(utc);
+            
+            RtcmV3Helper.GetFromTime(time, ref week, ref tow);
+            
+            var hour = Math.Floor(tow / 3600.0);
+            var sec = tow - hour * 3600.0;
+            if (zcnt < sec - 1800.0) zcnt += 3600.0;
+            else if (zcnt > sec + 1800.0) zcnt -= 3600.0;
+
+            return RtcmV3Helper.GetFromGps(week, hour * 3600 + zcnt);
+        }
+
+        private double? GetUdre(byte rsHealth)
+        {
+            switch (rsHealth)
+            {
+                case 0:
+                    return 1.0;
+                case 1:
+                    return 0.75;
+                case 2:
+                    return 0.5;
+                case 3:
+                    return 0.3;
+                case 4:
+                    return 0.2;
+                case 5:
+                    return 0.1;
+                case 6:
+                    return null;
+                case 7:
+                    return 0.0;
+                default:
+                    return null;
+            }
+        }
+
+        private const byte SyncByte = 0x66;
+
+        public override string ProtocolId => RtcmV2Parser.GnssProtocolId;
+
+        public override int GetMaxByteSize()
+        {
+            return (2 /* HEADER */ + 31 /* MAX DATA STRING */) * 3 /* bytes each */;
+        }
+
+        public override uint Serialize(byte[] buffer, uint offsetBits)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override uint Deserialize(byte[] buffer, uint offsetBits)
+        {
+            var bitIndex = offsetBits;
+
+            var preamble = (byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 8); bitIndex += 8;
+            if (preamble != SyncByte)
+            {
+                throw new Exception($"Deserialization RTCMv2 message failed: want {SyncByte:X}. Read {preamble:X}");
+            }
+
+            var msgType = RtcmV3Helper.GetBitU(buffer, bitIndex, 6); bitIndex += 6;
+            if (msgType != MessageId)
+            {
+                throw new Exception($"Deserialization RTCMv2 message failed: want message number '{MessageId}'. Read = '{msgType}'");
+            }
+
+            ReferenceStationId = (ushort)RtcmV3Helper.GetBitU(buffer, bitIndex, 10); bitIndex += 10;
+
+            ZCount = RtcmV3Helper.GetBitU(buffer, bitIndex, 13) * 0.6; bitIndex += 13;
+
+            if (ZCount >= 3600.0)
+            {
+                throw new Exception($"RTCMv2 Modified Z-count error: zcnt={ZCount}");
+            }
+            UTC = adjhour(ZCount);
+            
+            SequenceNumber = (byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 3); bitIndex += 3;
+            // if (SequenceNumber - rtcm->seqno != 1 && SequenceNumber - rtcm->seqno != -7)
+            // {
+            //     trace(2, "rtcm2 message outage: seqno=%d->%d\n", rtcm->seqno, SequenceNumber);
+            // }
+
+
+            PayloadLength = (byte)(RtcmV3Helper.GetBitU(buffer, bitIndex, 5) * 3); bitIndex += 5;
+            if (PayloadLength > (buffer.Length - 6 /* header 48 bit*/))
+            {
+                throw new Exception($"Deserialization RTCMv2 message failed: length too small. Want '{PayloadLength}'. Read = '{buffer.Length - 6}'");
+            }
+
+            Udre = GetUdre((byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 3)); bitIndex += 3;
+            
+            return bitIndex - offsetBits;
+        }
+
+        public double? Udre { get; set; }
+
+        public byte PayloadLength { get; set; }
+
+        public byte SequenceNumber { get; set; }
+
+        public DateTime UTC { get; set; }
+
+        public double ZCount { get; set; }
+
+        public ushort ReferenceStationId { get; set; }
+
+        public abstract ushort MessageId { get; }
+    }
+
+    public class RtcmV2Message1 : RtcmV2MessageBase
+    {
+        public const int RtcmMessageId = 1;
+
+        public override ushort MessageId => RtcmMessageId;
+
+        public override uint Deserialize(byte[] buffer, uint offsetBits)
+        {
+            var bitIndex = offsetBits + base.Deserialize(buffer, offsetBits);
+
+            var itmCnt = PayloadLength / 5;
+            ObservationItems = new DGpsObservationItem[itmCnt];
+
+            for (var i = 0; i < itmCnt; i++)
+            {
+                var item = new DGpsObservationItem();
+                bitIndex += item.Deserialize(buffer, bitIndex);
+                ObservationItems[i] = item;
+            }
+            
+            return bitIndex - offsetBits;
+        }
+
+        public DGpsObservationItem[] ObservationItems { get; set; }
+    }
+
+    public class DGpsObservationItem : ISerializable
+    {
+        public int GetMaxByteSize()
+        {
+            return 5;
+        }
+
+        public uint Serialize(byte[] buffer, uint offsetBits)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint Deserialize(byte[] buffer, uint offsetBits)
+        {
+            var bitIndex = offsetBits;
+
+            var fact = (byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 1); bitIndex += 1;
+            var udre = (byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 2); bitIndex += 2;
+            var prn = (byte)RtcmV3Helper.GetBitU(buffer, bitIndex, 5); bitIndex += 5;
+            var prc = RtcmV3Helper.GetBits(buffer, bitIndex, 16); bitIndex += 16;
+            var rrc = RtcmV3Helper.GetBits(buffer, bitIndex, 8); bitIndex += 8;
+            var iod = RtcmV3Helper.GetBits(buffer, bitIndex, 8); bitIndex += 8;
+
+            if (prn == 0) prn = 32;
+
+            Prn = prn;
+
+            if (prc == 0x80000000 || rrc == 0xFFFF8000)
+            {
+                Prc = 0.0;
+                Rrc = 0.0;
+            }
+            else
+            {
+                Prc = prc * (fact == 1 ? 0.32 : 0.02);
+                Rrc = rrc * (fact == 1 ? 0.032 : 0.002);
+            }
+            SatelliteId = RtcmV3Helper.satno(NavigationSystemEnum.SYS_GPS, prn);
+            Iod = iod;
+            Udre = udre;
+
+            return bitIndex - offsetBits;
+        }
+
+        public int SatelliteId { get; set; }
+
+        public byte Prn { get; set; }
+
+        public double Prc { get; set; }
+
+        public double Rrc { get; set; }
+
+        public int Iod { get; set; }
+
+        public byte Udre { get; set; }
+    }
+}
---
+++ src/Asv.Gnss/Parsers/RTCMv2/RtcmV2Parser.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asv.Gnss
+{
+    public class RtcmV2Parser : GnssParserBase
+    {
+        private const byte SyncByte = 0x66;
+
+        private readonly Dictionary<ushort, Func<RtcmV2MessageBase>> _dict = new Dictionary<ushort, Func<RtcmV2MessageBase>>();
+        private readonly IDiagnosticSource _diag;
+        private State _state;
+
+        public const string GnssProtocolId = "RTCMv2";
+
+        private readonly byte[] _buffer = new byte[33 * 3]; /* message buffer */
+        private uint _word;      /* word buffer for rtcm 2 */
+        private int _readedBytes;          /* number of bytes in message buffer */
+        private int _readedBits;           /* number of bits in word buffer */
+        private int _len;            /* message length (bytes) */
+
+        private enum State
+        {
+            Sync,
+            Preamb1,
+            Preamb2,
+            Payload
+        }
+
+        private bool decode_word(uint word, byte[] data, int offset)
+        {
+            var hamming = new uint[] {0xBB1F3480, 0x5D8F9A40, 0xAEC7CD00, 0x5763E680, 0x6BB1F340, 0x8B7A89C0};
+            uint parity = 0;
+            
+            if ((word & 0x40000000) != 0) word ^= 0x3FFFFFC0;
+
+            for (var i = 0; i < 6; i++)
+            {
+                parity <<= 1;
+                for (var w = (word & hamming[i]) >> 6; w != 0; w >>= 1) 
+                    parity ^= w & 1;
+            }
+            if (parity != (word & 0x3F)) return false;
+
+            for (var i = 0; i < 3; i++) data[i + offset] = (byte)(word >> (22 - i * 8));
+            return true;
+        }
+
+        public override string ProtocolId => GnssProtocolId;
+
+        public RtcmV2Parser(IDiagnostic diag)
+        {
+            _diag = diag[GnssProtocolId];
+        }
+
+        public override bool Read(byte data)
+        {
+            if ((data & 0xC0) != 0x40) return false; /* ignore if upper 2bit != 01 */
+
+            /* decode 6-of-8 form */
+            _word = (uint) ((_word << 6) + (data & 0x3F));
+            
+            switch (_state)
+            {
+                case State.Sync:
+                    var preamb = (byte)(_word >> 22);
+                    if ((_word & 0x40000000) != 0) preamb ^= 0xFF; /* decode preamble */
+                    if (preamb == SyncByte)
+                    {
+                        _state = State.Preamb1;
+                        goto case State.Preamb1;
+                    }
+                    break;
+                case State.Preamb1:
+                    if (decode_word(_word, _buffer, 0))
+                    {
+                        _state = State.Preamb2;
+                        _readedBytes = 3; _readedBits = 0;
+                    }
+                    else
+                    {
+                        _state = State.Sync;
+                        _diag.Int["crc err"]++;
+                        InternalOnError(new GnssParserException(ProtocolId, $"RTCMv2 parity error"));
+                    }
+                    break;
+                case State.Preamb2:
+                    _readedBits += 6;
+                    if (_readedBits >= 30)
+                    {
+                        _readedBits = 0;
+                        /* check parity */
+                        if (decode_word(_word, _buffer, _readedBytes))
+                        {
+                            _readedBytes += 3;
+                            _len = (_buffer[5] >> 3) * 3 + _readedBytes;
+                            _state = State.Payload;
+                        }
+                        else
+                        {
+                            _readedBytes = 0;
+                            _word &= 0x3;
+                            _state = State.Sync;
+                            _diag.Int["crc err"]++;
+                            InternalOnError(new GnssParserException(ProtocolId, $"RTCMv2 crc error"));
+                        }
+                    }
+                    break;
+                case State.Payload:
+                    _readedBits += 6;
+                    if (_readedBits >= 30)
+                    {
+                        _readedBits = 0;
+                        /* check parity */
+                        if (decode_word(_word, _buffer, _readedBytes))
+                        {
+                            _readedBytes += 3;
+                            if (_readedBytes >= _len)
+                            {
+                                _readedBytes = 0;
+                                _word &= 0x3;
+
+                                /* decode rtcm2 message */
+                                ParsePacket(_buffer);
+                                Reset();
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            _readedBytes = 0;
+                            _word &= 0x3;
+                            _state = State.Sync;
+                            _diag.Int["crc err"]++;
+                            InternalOnError(new GnssParserException(ProtocolId, $"RTCMv2 crc error"));
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return false;
+        }
+
+        #region Alt version Read
+
+        public bool ReadOld(byte data)
+        {
+            // trace(5,"input_rtcm2: data=%02x\n",data);
+
+            if ((data & 0xC0) != 0x40) return false; /* ignore if upper 2bit != 01 */
+
+            for (var i = 0; i < 6; i++, data >>= 1)
+            {
+                /* decode 6-of-8 form */
+                _word = (uint)((_word << 1) + (data & 1));
+
+                /* synchronize frame */
+                if (_readedBytes == 0)
+                {
+                    var preamb = (byte)(_word >> 22);
+                    if ((_word & 0x40000000) != 0) preamb ^= 0xFF; /* decode preamble */
+                    if (preamb != SyncByte) continue;
+
+                    /* check parity */
+                    if (!decode_word(_word, _buffer, 0)) continue;
+                    _readedBytes = 3; _readedBits = 0;
+                    continue;
+                }
+                if (++_readedBits < 30) continue;
+                else _readedBits = 0;
+
+
+                /* check parity */
+                if (!decode_word(_word, _buffer, _readedBytes))
+                {
+                    // trace(2, "rtcm2 partity error: i=%d word=%08x\n", i, rtcm->word);
+                    _readedBytes = 0; _word &= 0x3;
+                    continue;
+                }
+                _readedBytes += 3;
+                if (_readedBytes == 6) _len = (_buffer[5] >> 3) * 3 + 6;
+                if (_readedBytes < _len) continue;
+                _readedBytes = 0;
+                _word &= 0x3;
+
+                /* decode rtcm2 message */
+                ParsePacket(_buffer);
+                Reset();
+                return true;
+            }
+            return false;
+
+        }
+
+        #endregion
+
+        public void Register(Func<RtcmV2MessageBase> factory)
+        {
+            var testPckt = factory();
+            _dict.Add(testPckt.MessageId, factory);
+        }
+
+        private void ParsePacket(byte[] data)
+        {
+            var msgType = (ushort)RtcmV3Helper.GetBitU(data, 8, 6);
+            _diag.Speed[msgType.ToString()].Increment(1);
+
+            if (_dict.TryGetValue(msgType, out var factory) == false)
+            {
+                _diag.Int["unk err"]++;
+                InternalOnError(new GnssParserException(ProtocolId, $"Unknown RTCMv2 packet message number [MSG={msgType}]"));
+                return;
+            }
+
+            var message = factory();
+
+            try
+            {
+                message.Deserialize(data);
+            }
+            catch (Exception e)
+            {
+                _diag.Int["parse err"]++;
+                InternalOnError(new GnssParserException(ProtocolId, $"Parse RTCMv2 packet error [MSG={msgType}]", e));
+            }
+
+            try
+            {
+                InternalOnMessage(message);
+            }
+            catch (Exception e)
+            {
+                _diag.Int["pub err"]++;
+                InternalOnError(new GnssParserException(ProtocolId, $"Parse RTCMv2 packet error [MSG={msgType}]", e));
+            }
+
+        }
+
+        public override void Reset()
+        {
+            _state = State.Sync;
+        }
+    }
+}
