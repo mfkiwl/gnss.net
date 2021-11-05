717245351ec56c722b0eab925f952b9171862072
asvol <248997@gmail.com>
2021-11-05 12:34:47 +0500

    Add GPS raw subframe add

---
+++ src/Asv.Gnss/Parsers/RawGPS/Subframes/GpsSubframe1.cs
@@ -0,0 +1,16 @@
+﻿namespace Asv.Gnss
+{
+    public class GpsSubframe1: GpsSubframeBase
+    {
+        public override byte SubframeId => 1;
+
+        public override void Deserialize(byte[] dataWithoutParity)
+        {
+            base.Deserialize(dataWithoutParity);
+            var startBitIndex = 24U + 24U;
+            WeekNumber = GpsRawHelper.GetBitU(dataWithoutParity, startBitIndex, 10);
+        }
+
+        public uint WeekNumber { get; set; }
+    }
+}
